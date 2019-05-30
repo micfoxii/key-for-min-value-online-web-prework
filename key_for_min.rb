ca53eb66a9bfc5d129d4b8ffd46886a830786444
Michael Fox <michael.c.fox.ii@gmail.com>
2019-05-30 18:51:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,14 @@
 def key_for_min_value(name_hash)
   smallest_value = nil
   smallest_key = nil
-name_hash.each do |key, value|
-  if smallest_value == nil || value < smallest_value
-    smallest_value = value
-    smallest_key = key
+name_hash.each do |k, v|
+  if smallest_value == nil
+    smallest_value = v
+    smallest_key = k
+  elsif smallest_value > v
+    smallest_value = v
+    smallest_key = k
 end
 end
-lowest_key
+smallest_key
 end
